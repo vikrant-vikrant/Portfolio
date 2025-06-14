07d84f551cd10419cb1a6ce3bf8eaeb3b21f1b8c
sidharth <232006sidharth@gmail.com>
2025-06-14 14:44:32 +0530

    add details on heropage

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 VANTA.GLOBE({
   el: ".content",
-  mouseControls: true,
+  mouseControls: false,
+  // mouseControls: true,
   touchControls: true,
   gyroControls: false,
   minHeight: 200.0,
   minWidth: 200.0,
   scale: 1.0,
   scaleMobile: 1.0,
-  color: 0xff0000,
-  color2: 0x00ffff,
-  backgroundColor: 0x000000,
+  color: 0x054569,
+  color2: 0xced7e0,
+  backgroundColor: 0x1f1f1f,
 });
